995e5123db66ae9999001ce6c3af9179601f20ad
manishkyadavv <manish.reachout@gmail.com>
2022-08-23 18:03:08 +0530

    modified

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 import 'components/body.dart';
 
 // ignore: use_key_in_widget_constructors
-class LoginSuccessScreen extends StatelessWidget {
+class LoginSuccessScreen extends StatefulWidget {
   static String routeName = "/login_success";
+
+  @override
+  State<LoginSuccessScreen> createState() => _LoginSuccessScreenState();
+}
+
+class _LoginSuccessScreenState extends State<LoginSuccessScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
