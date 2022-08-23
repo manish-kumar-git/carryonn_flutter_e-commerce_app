e0e96df7f86719a062ca8322df01a6cd696c213d
manishkyadavv <manish.reachout@gmail.com>
2022-08-23 18:02:48 +0530

    modified

--- lib/screens/login_success/components/body.dart
+++ lib/screens/login_success/components/body.dart
@@ -7,34 +7,36 @@ import 'package:shop_app/size_config.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.04),
-        Image.asset(
-          "assets/images/success.png",
-          height: SizeConfig.screenHeight * 0.4, //40%
-        ),
-        SizedBox(height: SizeConfig.screenHeight * 0.08),
-        Text(
-          "Login Success",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(30),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(height: SizeConfig.screenHeight * 0.03),
+          Image.asset(
+            "assets/images/success.png",
+            height: SizeConfig.screenHeight * 0.4, //30%
           ),
-        ),
-        const Spacer(),
-        SizedBox(
-          width: SizeConfig.screenWidth * 0.6,
-          child: DefaultButton(
-            text: "Back to home",
-            press: () {
-              Navigator.pushNamed(context, HomeScreen.routeName);
-            },
+          SizedBox(height: SizeConfig.screenHeight * 0.08),
+          Text(
+            "Login Success",
+            style: TextStyle(
+              fontSize: getProportionateScreenWidth(30),
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        const Spacer(),
-      ],
+          const Spacer(),
+          SizedBox(
+            width: SizeConfig.screenWidth * 0.6,
+            child: DefaultButton(
+              text: "Carry On Shopping",
+              press: () {
+                Navigator.pushNamed(context, HomeScreen.routeName);
+              },
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
